4b5842e847f3df91679958f7ce9b24eb5916619f
UndrscoreEX <ethan.heikell.official@gmail.com>
2023-02-02 20:09:22 +0900

    forgot to add domain to allowed hosts

--- high_spec/high_spec/urls.py
+++ high_spec/high_spec/urls.py
@@ -10,3 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('spec/', include('spec_calculator.urls')),
 ] + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+# the above is when you want to serve static files via Django for production servers (only suitable for small apps)
+# better to use nginx 
